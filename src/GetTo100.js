a689ec76f9c91926f52cf394e72a5bcc2d13a31f
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-03-22 14:39:20 +0200

    get to 100 comp

---
+++ src/GetTo100.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Game from "./Game";
+import PlayerList from "./PlayerList";
+
+class GetTo100 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: [
+        { name: "Player 1", gamesPlayed: 0, bestScore: null, score: 5 },
+        { name: "Player 2", gamesPlayed: 0, bestScore: null, score: 7 },
+        { name: "Player 3", gamesPlayed: 0, bestScore: null, score: 9 },
+      ],
+      activePlayerIndex: 0,
+    };
+  }
+
+  handleGameCompletion = (playerIndex) => {//Father Function
+    const { players, activePlayerIndex } = this.state;
+    const player = players[playerIndex];
+    //player.gamesPlayed += 1;
+    // if (!player.bestScore || score < player.bestScore) {
+    //   player.bestScore = score;
+    // }
+    this.setActivePlayerIndex();
+
+    this.setState({ players });
+  };
+
+  setActivePlayerIndex = (newScore) => {
+    const { players, activePlayerIndex } = this.state;
+    players[activePlayerIndex].score = newScore;
+    const nextActivePlayerIndex = (activePlayerIndex + 1) % players.length;
+    this.setState({ activePlayerIndex: nextActivePlayerIndex, players }); ///////////////event!!
+  };
+
+  render() {
+    const { players, activePlayerIndex } = this.state;
+
+    return (
+      <div className="app">
+        <PlayerList players={players} />
+        <Game
+          players={players}
+          activePlayerIndex={activePlayerIndex}
+          onGameCompletion={this.handleGameCompletion}
+          setActivePlayerIndex={this.setActivePlayerIndex}
+          score={this.state.players[this.state.activePlayerIndex].score}
+        />
+      </div>
+    );
+  }
+}
+
+export default GetTo100;
